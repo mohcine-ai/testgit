cb22a26f190f0cda2dfa1e217c603b2d8a85edde
mohcine-ai <khalilmohcine@icloud.com>
2024-09-28 17:36:42 +0100

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by Mohcine Khalil on 28/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
